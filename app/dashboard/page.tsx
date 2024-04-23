2c7abd52a71da8decc27cda3b23de8648cbf0b87
Ivan Tsarehorodtsev <i.tsarehorodtsev@kasta.software>
2024-04-23 20:34:46 +0300

    test: test imports

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,8 @@
 import Image from "next/image";
-// import { HERO_DATA } from "./hero-data";
-// import HeroItem from "./components/common/hero-item";
+import { HERO_DATA } from "./hero-data";
+import HeroItem from "./components/common/hero-item";
 
 import classes from "./page.module.css";
-//import { Typography } from "@mui/material";
 
 const DashboardPage = () => {
   return (
@@ -22,8 +21,7 @@ const DashboardPage = () => {
       </div>
 
       <section className={classes.campaignHeroesContainer}>
-        {/* <Typography
-          variant="h4"
+        <h4
           style={{
             textAlign: "center",
             marginBottom: "48px",
@@ -32,9 +30,9 @@ const DashboardPage = () => {
           }}
         >
           Знайомтеся з нашими персонажами
-        </Typography> */}
+        </h4>
 
-        {/* <div style={{ margin: "0 auto" }}>
+        <div style={{ margin: "0 auto" }}>
           {HERO_DATA.map((hero, idx) => {
             return (
               <HeroItem
@@ -44,7 +42,7 @@ const DashboardPage = () => {
               />
             );
           })}
-        </div> */}
+        </div>
       </section>
     </>
   );

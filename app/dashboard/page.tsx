daee7fa3eb54e135d8f176fef9e0e8a349be1fed
Ivan Tsarehorodtsev <i.tsarehorodtsev@kasta.software>
2024-04-23 20:39:17 +0300

    test: test imports

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,8 @@
+"use client";
+
 import Image from "next/image";
 import { HERO_DATA } from "./hero-data";
-// import HeroItem from "./components/common/hero-item";
+import HeroItem from "./components/common/hero-item";
 
 import classes from "./page.module.css";
 
@@ -33,7 +35,7 @@ const DashboardPage = () => {
         </h4>
 
         <div style={{ margin: "0 auto" }}>
-          {/* {HERO_DATA.map((hero, idx) => {
+          {HERO_DATA.map((hero, idx) => {
             return (
               <HeroItem
                 key={hero.name}
@@ -41,7 +43,7 @@ const DashboardPage = () => {
                 hero={hero}
               />
             );
-          })} */}
+          })}
         </div>
       </section>
     </>
